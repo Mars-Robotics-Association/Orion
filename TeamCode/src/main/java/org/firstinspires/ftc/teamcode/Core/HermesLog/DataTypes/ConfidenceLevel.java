1275b9e12c0f9183b0e82adb1a08966e57cba134
Luke <lbkman36@gmail.com>
2021-10-07 20:12:58 -0400

    Tried to add stuff for sending full image

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/HermesLog/DataTypes/ConfidenceLevel.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.Core.HermesLog.DataTypes;
+
+public class ConfidenceLevel {
+    public ConfidenceLevel(double conf){confidence=conf;}
+    public double confidence = 0.0;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/HermesLog/DataTypes/FullIMG.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Core.HermesLog.DataTypes;
+
+public class FullIMG
+{
+    public FullIMG(String image2){
+        img2 = image2;
+    }
+
+    public String img2 = "";
+}
