5f92cb2f738af7c83e1d193d5dad870c3ea28f46
isaacthoman <isaacthoman@outlook.com>
2021-10-14 20:51:27 -0400

    Andrew opmode and controllers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Opportunity/Andrew.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode._RobotCode.Opportunity;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Core.HermesLog.HermesLog;
+import org.firstinspires.ftc.teamcode.Core.MechanicalControlToolkit.Chassis.MecanumBaseControl;
+import org.firstinspires.ftc.teamcode.Orion.NavProfiles.NavigationProfile;
+
+@Config
+public class Andrew extends MecanumBaseControl
+{
+    public Andrew(OpMode setOpMode, NavigationProfile setNavProfile, HermesLog setLog, boolean useChassis, boolean usePayload, boolean useNavigator) {
+        super(setOpMode, setNavProfile, setLog, useChassis, usePayload, useNavigator);
+        super.InitCoreRobotModules();
+    }
+
+    public void Start(){
+        super.StartCoreRobotModules();
+    }
+}
