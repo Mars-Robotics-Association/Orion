a8cef2a856c615eb667292a2d7c6b4bf0ccb5514
Owen <owencanderson@icloud.com>
2021-11-18 18:19:35 -0500

    Field state objects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orion/FieldState/FieldObject.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orion/FieldState/FieldObject.java
@@ -7,12 +7,14 @@ public class FieldObject
     public String category;
     public enum ObjectType {
         STATIC,//object never moves, state should never be deleted
-        SEMISTATIC,//object rarely moves, state should persist unless changed
+        SEMISTATIC,//object rarely moves and always exists, state should persist unless changed
         DYNAMIC,//object will move, state should re-assess whenever necessary (eg. camera points at it)
         PROBABILITY//object might exist, only on list when detected, deleted whenever not seen
     }
+    public ObjectType objectType;
     public double[] poseToRobot = {0,0,0};
     public double[] poseToGlobal = {0,0,0};
     public double poseToRobotCertainty = 1;
     public double boundsRadius = 1;
+    public int timeUpdated = 0;
 }
