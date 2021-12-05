d0581b35cf9a73ef521eee96887c51287b123161
Luke <lbkman36@gmail.com>
2021-12-05 18:57:57 -0500

    Some universal classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpenCVMethods.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+public class OpenCVMethods {
+    private OpMode opmode;
+    private TFObjectDetector tfod;
+
+    public void GetImage()
+    {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orion/NavModules/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orion/NavModules/Camera.java
@@ -1,6 +1,156 @@
 package org.firstinspires.ftc.teamcode.Orion.NavModules;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
 class Camera
 {
+    private OpMode opmode;
+    private WebcamName webcamname;
+    private VuforiaLocalizer vuforia;
+    private VuforiaTrackables trackables;
+    private TFObjectDetector tfod;
 
+    public VuforiaLocalizer GetVuforia() {return vuforia;}
+
+    private int cameraMonitorViewID;
+    final String VLK = "AeZ+Eyv/////AAABmfcFKgZ5NkXfgqEeyUnIJMIHuzBJhNTY+sbZO+ChF7mbo1evegC5fObZ830PRBTGTM6jbp+1XXCzx3XhY1kaZevEQXNpAKhXU9We0AMlp1mhnAUjGI2sprJZqJIfFGxkK598u8Bj3qQy4+PlCrk+Od/tAGs8dqAAsZPp4KpczFQttxMBC5JZNeIbIFP57InXOeJgyeH1sXK+R2i6nPfOFRvHJjdQaLWdAasv7i3b0RH5ctG7Ky7J9g9BPYI03kkChCJkbPg03XnoqCcC7rEpAk3n8a9CqtwTUu57Sy0jCDUd2O6X9kHjZ5ZmS0I3O0YSzX3Jp2ppTE2kDS2I9zBYEmuEqkMjItxd52oES0Ij0rZm";
+
+
+    public Camera(OpMode opMode, String webCam) {
+        opmode = opMode;
+
+        webcamname = opmode.hardwareMap.get(WebcamName.class,webCam);
+        cameraMonitorViewID = opmode.hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", opmode.hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewID);
+
+        parameters.vuforiaLicenseKey = VLK;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.cameraName = webcamname;
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        trackables = GetVuforia().loadTrackablesFromAsset("UltimateGoal");
+
+        opMode.telemetry.update();
+
+        trackables.activate();
+
+        //Init
+        initTfod();
+        if (tfod != null)tfod.activate();
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = opmode.hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", opmode.hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset("FreightFrenzy.tflite", "Ball", "Cube");
+        tfod.setZoom(2, 16.0/9.0);
+    }
+
+    public void stopTfod(){
+        if (tfod != null) tfod.shutdown();
+    }
+
+    public List<Recognition> GetRecognitions() {
+        if (tfod != null) {
+            List<Recognition> recognitions = tfod.getRecognitions();
+            if (recognitions != null) {
+                opmode.telemetry.addData("# Object Detected", recognitions.size());
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                for (Recognition recognition : recognitions) {
+                    opmode.telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                    opmode.telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                            recognition.getLeft(), recognition.getTop());
+                    opmode.telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                            recognition.getRight(), recognition.getBottom());
+                }
+            }
+            return recognitions;
+        }
+        else return null;
+    }
+
+    public void PrintTFTelemetry(){
+        List<Recognition> items = GetRecognitions();
+        int amount = 0;
+        if(items != null){
+            for (Recognition r : items) {
+                opmode.telemetry.addLine("Top: "+r.getTop()+" | Bottom: "+r.getBottom()+" | Left: "+r.getLeft()+"| Right: "+r.getRight());
+            }
+        }
+    }
+    public double[] GetPoseToCamera(int vumarkIndex) {
+
+        double[] data = {0.0,0.0,0.0,0.0,0.0,0.0};
+        VuforiaTrackable vumark = trackables.get(vumarkIndex);
+
+        VuforiaTrackable vuMark = vumark;
+        if (vuMark != null) {
+
+            /* Found an instance of the template. In the actual game, you will probably
+             * loop until this condition occurs, then move on to act accordingly depending
+             * on which VuMark was visible. */
+            opmode.telemetry.addData("VuMark", "%s visible", vuMark.getName());
+
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)vumark.getListener()).getFtcCameraFromTarget();
+            opmode.telemetry.addData("Pose", format(pose));
+
+            /* We further illustrate how to decompose the pose into useful rotational and
+             * translational components */
+            if (pose != null) {
+                VectorF trans = pose.getTranslation();
+                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+                // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                double tX = trans.get(0);
+                double tY = trans.get(1);
+                double tZ = trans.get(2);
+
+                // Extract the rotational components of the target relative to the robot
+                double rX = rot.firstAngle;
+                double rY = rot.secondAngle;
+                double rZ = rot.thirdAngle;
+
+                double dist = Math.sqrt(Math.pow(Math.abs(tX),2)+Math.pow(Math.abs(tY),2));//tX use to by tY, this was switched because the camera uses portrait mode
+                double rZreal = Math.toDegrees(Math.atan(tX/tZ));
+                //opMode.telemetry.addData("Vumark",dist + " milimeters away");
+                //opMode.telemetry.addData("Vumark",-1*tX+" milimeters high");
+                data[0] = tX / 25.4f;
+                data[1]= tY / 25.4f;
+                data[2] = tZ / 25.4f;
+                data[3] = dist / 25.4f;
+                data[4] = rZreal;
+                data[5] = rY;
+            }
+
+        }
+        else {
+            opmode.telemetry.addData("VuMark", "not visible");
+        }
+        return data;
+
+    }
+    String format(OpenGLMatrix transformationMatrix) {
+        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+    }
 }
