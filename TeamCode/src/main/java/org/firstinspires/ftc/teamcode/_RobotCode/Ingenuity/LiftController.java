ff06e8d5b700f2c01add6d31c5352a0cd2b9af0a
Erik <erik@erik-anderson.net>
2021-12-29 20:54:11 -0500

    Ingenuity changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Ingenuity/LiftController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Ingenuity/LiftController.java
@@ -16,7 +16,7 @@ class LiftController {
     public LiftController(int lowerBounds_e, int upperBounds_e, boolean raiseBoundErrors_e) {
         lowerBounds = lowerBounds_e;
         upperBounds = upperBounds_e;
-        speed = 100;
+        speed = 0.1;
         raiseBoundErrors = raiseBoundErrors_e;
     }
 
@@ -30,13 +30,15 @@ class LiftController {
     public LiftController(int lowerBounds_e, int upperBounds_e) {
         lowerBounds = lowerBounds_e;
         upperBounds = upperBounds_e;
-        speed = 100;
+        speed = 0.1;
         raiseBoundErrors = false;
     }
 
     public void Init(OpMode opMode, String motor) {
         lift = (DcMotorEx) opMode.hardwareMap.dcMotor.get(motor);
         lift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        liftOrion = (DcMotor) opMode.hardwareMap.dcMotor.get(motor);
+        liftOrion.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     public void setPosition(int height) throws BruhException {
@@ -46,6 +48,7 @@ class LiftController {
             lift.setTargetPosition(height);
             lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
             lift.setVelocity(speed);
+
             
         } else if (height < lowerBounds){
             message = String.format("%s is below the lower bounds of %s ticks", height, lowerBounds);
@@ -57,9 +60,21 @@ class LiftController {
         }
     }
 
+    public void go(double myspeed){
+        liftOrion.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftOrion.setPower(myspeed);
+
+    }
+
     public void setSpeed(double eSpeed){
         speed = eSpeed;
     }
+
+    public void rawMove(double eSpeed) {
+        liftOrion.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        liftOrion.setPower(eSpeed);
+    }
+
 
     public void toLowerBounds(){
         lift.setTargetPosition(lowerBounds);
@@ -73,10 +88,17 @@ class LiftController {
         lift.setVelocity(speed);
     }
 
+    public void LockArm(){
+        lift.setTargetPosition(lift.getCurrentPosition());
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setPower(speed);
+    }
+
     private int lowerBounds;
     private int upperBounds;
     private double speed;
     private boolean raiseBoundErrors;
 
     private DcMotorEx lift;
+    private DcMotor liftOrion ;
 }
