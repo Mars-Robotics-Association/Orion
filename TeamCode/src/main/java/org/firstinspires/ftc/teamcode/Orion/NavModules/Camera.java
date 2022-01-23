8c4c69827cc753aacf0afffd24062a1574e4c05c
Luke <lbkman36@gmail.com>
2022-01-23 16:50:14 -0500

    camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orion/NavModules/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Orion/NavModules/Camera.java
@@ -383,21 +383,21 @@ public class Camera
     }
 
     public Bitmap GetImage() throws InterruptedException {
-        Image img = vuforia.getFrameQueue().take().getImage(1);
-//        VuforiaLocalizer.CloseableFrame frame = vuforia.getFrameQueue().take(); //takes the frame at the head of the queue
-//        long numImages = frame.getNumImages();
-//        Image rgb=null;
-//        for (int i = 0; i < numImages; i++) {
-//            if (frame.getImage(i).getFormat() == PIXEL_FORMAT.RGB565) {
-//                rgb = frame.getImage(i);
-//                break;
-//            }
-//        }
-//
-        Bitmap bmp = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.RGB_565);
-//        bmp.copyPixelsFromBuffer(rgb.getPixels());
-//        frame.close();
+//        Image img = vuforia.getFrameQueue().take().getImage(1);
+        VuforiaLocalizer.CloseableFrame frame = vuforia.getFrameQueue().take(); //takes the frame at the head of the queue
+        long numImages = frame.getNumImages();
+        Image img=null;
+        for (int i = 0; i < numImages; i++) {
+            if (frame.getImage(i).getFormat() == PIXEL_FORMAT.RGB565) {
+                img = frame.getImage(i);
+                break;
+            }
+        }
 
+
+        Bitmap bmp = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.RGB_565);
+        bmp.copyPixelsFromBuffer(img.getPixels());
+        frame.close();
         //Bitmap bmp = frameQueue.poll();
         return bmp;
     }
