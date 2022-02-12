7bb22e2a3a32b72a45656a43debbf2fe01d0c62f
Luke <lbkman36@gmail.com>
2022-02-11 21:20:09 -0500

    thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Curiosity/ArmLeveller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Curiosity/ArmLeveller.java
@@ -15,7 +15,7 @@ class ArmLeveller implements Runnable
 
     //levelling sensor
     double armSlowDistanceCM = 8;
-    double armResetDistanceCM = 1.5;
+    double armResetDistanceCM = 2;
     double armStorageLocation = 0.02;
 
     //intaking sensor
