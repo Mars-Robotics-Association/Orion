4375a2ffeb337d2a7db4aef1f400f52f7f3c5944
Isaac <isaac.hanson@marsroboticsassociation.org>
2022-12-08 19:17:32 -0500

    Fix formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Ingenuity/IngenuityAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Ingenuity/IngenuityAutonomous.java
@@ -12,8 +12,7 @@ import org.firstinspires.ftc.teamcode.Navigation.PurePursuit.path.PathPoint;
 
 @Autonomous(name = "Ingenuity Autonomous", group = "Ingenuity")
 @Config
-public class IngenuityAutonomous extends LinearOpMode
-{
+public class IngenuityAutonomous extends LinearOpMode {
     public static double speed = 0.3;
     IngenuityPowerPlayBot robot;
     //public DcMotor armMotor ;
@@ -22,7 +21,7 @@ public class IngenuityAutonomous extends LinearOpMode
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robot = new IngenuityPowerPlayBot(this,true,true,true);
+        robot = new IngenuityPowerPlayBot(this, true, true, true);
         robot.init();
         robot.getChassis().setHeadlessMode(true);
         //armMotor = hardwareMap.dcMotor.get("armMotor") ;
@@ -33,26 +32,27 @@ public class IngenuityAutonomous extends LinearOpMode
         robot.start();
 
         //go forward to the signal
-        while(! robot.getNavigator().goTowardsPose(18,3,0,0.22)&& !isStopRequested()) {
+        while (!robot.getNavigator().goTowardsPose(18, 3, 0, 0.22) && !isStopRequested()) {
             robot.update();
             printTelemetry();
+        }
 
         //go forward to the signal
-        goToPose(21,0,0);
+        goToPose(21, 0, 0);
         //read the signal
-        signalZone=robot.readSignal();
+        signalZone = robot.readSignal();
         //move forward
-        goToPose(25,0,0) ;
+        goToPose(25, 0, 0);
         //strafe to signal zone
-        switch(signalZone){
-            case 1 :
-                goToPose(23,-25,0);
+        switch (signalZone) {
+            case 1:
+                goToPose(23, -25, 0);
                 break;
             case 3:
-                goToPose(25,15,0);
+                goToPose(25, 15, 0);
                 break;
             default:
-                goToPose(23,0,0);
+                goToPose(23, 0, 0);
         }
 
         // Move in rectangle, clockwise around the post
@@ -73,9 +73,9 @@ public class IngenuityAutonomous extends LinearOpMode
     }
 
     private void goToPose(double x, double y, double angle) {
-        while(! robot.getNavigator().goTowardsPose(x, y, angle,0.25) && !isStopRequested()) {
+        while (!robot.getNavigator().goTowardsPose(x, y, angle, 0.25) && !isStopRequested()) {
             robot.update();
-            robot.opMode.telemetry.addData("Going to to ", "("+x+", "+y+", "+angle+")");
+            robot.opMode.telemetry.addData("Going to to ", "(" + x + ", " + y + ", " + angle + ")");
             robot.opMode.telemetry.update();
         }
     }
@@ -83,17 +83,18 @@ public class IngenuityAutonomous extends LinearOpMode
     public void autoDrive() {
         // Move in rectangle, clockwise around the post
         // Move forward to 16x, 0y
-        goToPose(20,0,0);
+        goToPose(20, 0, 0);
 
         // Strafe right to 16v, 16y
-        goToPose(20,20,0);
+        goToPose(20, 20, 0);
 
         // Move backward to 0x, 16y
-        goToPose(0,20,0);
+        goToPose(0, 20, 0);
 
         // strafe left to 0x, 0y
-        goToPose(0,0,0);
+        goToPose(0, 0, 0);
     }
+
     private void printTelemetry() {
         /*
         //CONTROLS
@@ -116,15 +117,15 @@ public class IngenuityAutonomous extends LinearOpMode
         telemetry.addLine();
         telemetry.addData("Zone: ", signalZone);
         telemetry.addLine("----DATA----");
-        telemetry.addData("color ",String.valueOf(robot.colorSensor.red()),String.valueOf(robot.colorSensor.green()),String.valueOf(robot.colorSensor.blue()));
+        telemetry.addData("color ", String.valueOf(robot.colorSensor.red()), String.valueOf(robot.colorSensor.green()), String.valueOf(robot.colorSensor.blue()));
         telemetry.addData("Gripper: ", robot.servoTarget);
         //telemetry.addData("Arm:     ", armMotor.getCurrentPosition());
         //Dead wheel positions
         telemetry.addLine("Dead wheel positions");
         double[] deadWheelPositions = robot.getNavigator().getDeadWheelPositions();
-        telemetry.addData("LEFT dead wheel:       ", deadWheelPositions[0]+" inches");
-        telemetry.addData("RIGHT dead wheel:      ", deadWheelPositions[1]+" inches");
-        telemetry.addData("HORIZONTAL dead wheel: ", deadWheelPositions[2]+" inches");
+        telemetry.addData("LEFT dead wheel:       ", deadWheelPositions[0] + " inches");
+        telemetry.addData("RIGHT dead wheel:      ", deadWheelPositions[1] + " inches");
+        telemetry.addData("HORIZONTAL dead wheel: ", deadWheelPositions[2] + " inches");
         //Odometry estimated pose
         telemetry.addLine();
         telemetry.addLine("Robot pose");
