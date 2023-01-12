7c950a6fb08b2dfa1c22acc4ec8ed2b1d713bdf8
Aaron Wollerton <aaronwollerton@gmail.com>
2023-01-12 18:23:49 -0500

    Recovered 1.16.4   Fixed inverted Drive Controls.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/MechanicalControlToolkit/Chassis/MecanumChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/MechanicalControlToolkit/Chassis/MecanumChassis.java
@@ -124,12 +124,12 @@ public class MecanumChassis
     public void driveWithGamepad(ControllerInput controllerInput, double driveSpeed, double turnSpeed, double speedMultiplier) {
         //MOVE if left joystick magnitude > 0.1
         if (controllerInput.CalculateLJSMag() > 0.1) {
-            rawDrive(controllerInput.CalculateLJSAngle()+inputOffset, controllerInput.CalculateLJSMag() * driveSpeed * speedMultiplier, controllerInput.GetRJSX() * turnSpeed * speedMultiplier);//drives at (angle, speed, turnOffset)
+            rawDrive(controllerInput.CalculateLJSAngle()+inputOffset, -controllerInput.CalculateLJSMag() * driveSpeed * speedMultiplier, controllerInput.GetRJSX() * turnSpeed * speedMultiplier);//drives at (angle, speed, turnOffset)
             opMode.telemetry.addData("Moving at ", controllerInput.CalculateLJSAngle());
         }
         //TURN if right joystick magnitude > 0.1 and not moving
         else if (Math.abs(controllerInput.GetRJSX()) > 0.1) {
-            rawTurn(controllerInput.GetRJSX() * turnSpeed * speedMultiplier);//turns at speed according to rjs1
+            rawTurn(-controllerInput.GetRJSX() * turnSpeed * speedMultiplier);//turns at speed according to rjs1
             opMode.telemetry.addData("Turning", true);
         }
         else {
