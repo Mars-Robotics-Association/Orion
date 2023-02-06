3397af1c5cd739bde157b76258b858e322a9b60c
Matt Hanson <sqlsourcerer@gmail.com>
2023-02-06 07:50:03 -0500

    coordinates copied from right version and adjusted for left differences

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Ingenuity/IngenuityAutonomousLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Ingenuity/IngenuityAutonomousLeft.java
@@ -9,36 +9,36 @@ public class IngenuityAutonomousLeft extends IngenuityAutonomous {
 
     @Override
     protected void posMedJunction() {
-
+        goToPose( 26.5, 1.5, 45, 10000, true);
     }
 
     @Override
     protected void posPreStack() {
-
+        goToPose(47, 0, -87, 2500, false);
     }
 
     @Override
     protected void posStack() {
-
+        goToPose(47, -14, -87, 10000, true);
     }
 
     @Override
     protected void posPostStack() {
-
+        goToPose(47, -2, -87, 1200, false);
     }
 
     @Override
     protected void posHighJunction() {
-
+        goToPose(54, 8, 36, 10000, true);
     }
 
     @Override
     protected void posPostHighJunction() {
-
+        goToPose(49, 3, 36, 1000, false);
     }
 
     @Override
     protected void posLowJunction() {
-
+        goToPose(48, 3.5, -127, 10000, true);
     }
 }
