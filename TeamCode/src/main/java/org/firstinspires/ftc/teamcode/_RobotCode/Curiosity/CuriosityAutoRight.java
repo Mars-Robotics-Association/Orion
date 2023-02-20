3e5f29360c0e2fbbce45d9dcdd95c2a9a9343505
Luke <lbkman36@gmail.com>
2023-02-20 18:36:20 -0500

    Auto but now with functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Curiosity/CuriosityAutoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Curiosity/CuriosityAutoRight.java
@@ -56,85 +56,25 @@ public class CuriosityAutoRight extends LinearOpMode {
         telemetry.addData("Position",coneSide);
         telemetry.update();
 
-        //resets arm
-
-        //places preload cone
-        if(coneSide==1) {
-            goToPose(8, -5, 0, 0.8);
-            turnTo(-45, speed);
-            deployCone(CuriosityPayload.Pole.LOW);
-            //sleep(300);
-            turnTo(0, speed);
-        }
-        goToPose(55,0,0,1);
-        goToPose(48,0,0,1);
-        turnTo(90,speed);
-
-        double conePickupX = 47;
-        double conePickupY = 25;
-
-        //raises arm to pick up cone
-        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
-        //picks up cone
-        pickUpCone(5);
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
-        goToPose(52, -6,-45,speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.HIGH);
-        //sleep(300);
-
-        //raises arm to pick up cone
-        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
-        //picks up cone
-        pickUpCone(4);
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX,(conePickupY-8),90,speed);//backs up a bit to clear stack
-        goToPose(44, 10,-180,speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.LOW);
-        //sleep(300);
-        goToPose(48,10,-180,speed);//back up a bit
-        turnTo(90,speed);
-
-        //raises arm to pick up cone
-        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
-        //picks up cone
-        pickUpCone(3);
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
-        goToPose(45, -5,-135,speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.MID);
-        //sleep(300);
-
-
-
-//        robot.getPayload().goToHeight(Old_CuriosityPayload.getPoleHeight(Old_CuriosityPayload.Pole.GROUND));
-
         //spot 1(green)
         if(coneSide==1) {
-            //go to left
-            goToPoseNoLR(48,-20,-180,1);
+            FirstStop();
         }
         //spot 2(purple)
         else if(coneSide==2){
-            //go to center
-            goToPoseNoLR(48,0,-180,1);
+            SecondStop();
         }
         //spot 3(orange)
         else{
-            //go to right
-            goToPoseNoLR(48,24,-180,1);
+            ThirdStop();
         }
         telemetry.addLine("DONE");
         telemetry.update();
         robot.stop();
         stop();
     }
+
+    enum Junction {UPPER,STACK,CENTER,FAR,LOWER};
 
     //move this somewhere else if it goes in a different class
     int getConeSide(Camera c) throws InterruptedException {
@@ -198,4 +138,244 @@ public class CuriosityAutoRight extends LinearOpMode {
         robot.getPayload().toggleGripper(false);
         robot.getPayload().update(robot.getPayload().pickupPose[0]+6,robot.getPayload().pickupPose[1]);
     }
+
+    CuriosityAutoLeft.Junction[] getOrder(int coneSide){
+        switch(coneSide){
+            case(1):
+                return new CuriosityAutoLeft.Junction[] {CuriosityAutoLeft.Junction.LOWER,CuriosityAutoLeft.Junction.UPPER, CuriosityAutoLeft.Junction.CENTER, CuriosityAutoLeft.Junction.STACK};
+            case(2):
+                return new CuriosityAutoLeft.Junction[] {CuriosityAutoLeft.Junction.LOWER,CuriosityAutoLeft.Junction.STACK, CuriosityAutoLeft.Junction.UPPER, CuriosityAutoLeft.Junction.CENTER};
+            case(3):
+                return new CuriosityAutoLeft.Junction[] {CuriosityAutoLeft.Junction.STACK, CuriosityAutoLeft.Junction.UPPER, CuriosityAutoLeft.Junction.CENTER, CuriosityAutoLeft.Junction.FAR};
+        }
+        return new CuriosityAutoLeft.Junction[] {CuriosityAutoLeft.Junction.LOWER, CuriosityAutoLeft.Junction.UPPER, CuriosityAutoLeft.Junction.CENTER, CuriosityAutoLeft.Junction.STACK};
+    }
+
+    double[] getCords(CuriosityAutoLeft.Junction j){
+        if(j== CuriosityAutoLeft.Junction.LOWER){
+            return new double[] {8,-5,0};
+        }
+        else if(j== CuriosityAutoLeft.Junction.UPPER)
+        {
+            return new double[] {52, -6,-45};
+        }
+        else if(j== CuriosityAutoLeft.Junction.STACK){
+            return new double[] {44, 10,-180};
+        }
+        else if(j== CuriosityAutoLeft.Junction.CENTER){
+            return new double[] {45, -5,-135};
+        }
+        //FAR
+        else{
+            return new double[] {45,-29,-135};
+        }
+    }
+
+    CuriosityPayload.Pole getHeight(CuriosityAutoLeft.Junction j){
+        if(j== CuriosityAutoLeft.Junction.LOWER){
+            return CuriosityPayload.Pole.LOW;
+        }
+        else if(j== CuriosityAutoLeft.Junction.UPPER)
+        {
+            return CuriosityPayload.Pole.HIGH;
+        }
+        else if(j== CuriosityAutoLeft.Junction.STACK){
+            return CuriosityPayload.Pole.LOW;
+        }
+        else if(j== CuriosityAutoLeft.Junction.CENTER){
+            return CuriosityPayload.Pole.MID;
+        }
+        //FAR
+        else{
+            return CuriosityPayload.Pole.HIGH;
+        }
+    }
+
+    void FirstStop() throws InterruptedException {
+        double conePickupX = 47;
+        double conePickupY = 25;
+        //places preload cone
+        goToPose(8,-5,0, 0.8);
+        turnTo(-45, speed);
+        deployCone(CuriosityPayload.Pole.LOW);
+        //sleep(300);
+        turnTo(0, speed);
+        goToPose(55, 0, 0, 1);
+        goToPose(48, 0, 0, 1);
+        //raises arm to pick up cone
+        turnTo(90, speed);
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(5);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
+        goToPose(52, -6,-45,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.HIGH);
+        //sleep(300);
+
+        //raises arm to pick up cone
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(4);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-8),90,speed);//backs up a bit to clear stack
+        goToPose(44, 10,-180,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.LOW);
+        //sleep(300);
+        goToPose(48,10,-180,speed);//back up a bit
+        turnTo(90,speed);
+
+        //raises arm to pick up cone
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(3);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
+        goToPose(45, -5,-135,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.MID);
+        //sleep(300);
+        goToPose(48, -5,-135,speed);//back up a bit
+        
+        //go to left
+        goToPoseNoLR(48,-20,-180,1);
+    }
+
+    void SecondStop() throws InterruptedException {
+        double conePickupX = 47;
+        double conePickupY = 25;
+        //places preload cone
+        goToPose(8,-5,0, 0.8);
+        turnTo(-45, speed);
+        deployCone(CuriosityPayload.Pole.LOW);
+        //sleep(300);
+        turnTo(0, speed);
+        goToPose(55, 0, 0, 1);
+        goToPose(48, 0, 0, 1);
+
+        //raises arm to pick up cone
+        turnTo(90, speed);
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(5);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
+        goToPose(45, -5,-135,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.MID);
+        //sleep(300);
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(4);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
+        goToPose(52, -6,-45,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.HIGH);
+        //sleep(300);
+
+        //raises arm to pick up cone
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(3);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-8),90,speed);//backs up a bit to clear stack
+        goToPose(44, 10,-180,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.LOW);
+        //sleep(300);
+        goToPose(48,10,-180,speed);//back up a bit
+        turnTo(90,speed);
+
+        //go to center
+        goToPoseNoLR(48,0,-180,1);
+    }
+
+    void ThirdStop() throws InterruptedException {
+        double conePickupX = 47;
+        double conePickupY = 25;
+        //places preload cone
+        goToPose(45, -5,-135,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.MID);
+        //sleep(300);
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(5);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-10),90,speed);//backs up a bit to clear stack
+        goToPose(52, -6,-45,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.HIGH);
+        //sleep(300);
+
+        //raises arm to pick up cone
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(4);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-8),90,speed);//backs up a bit to clear stack
+        goToPose(44, 10,-180,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.LOW);
+        //sleep(300);
+
+        //raises arm to pick up cone
+        goToPose(conePickupX, conePickupY,90,speed);//goes to the stack
+        //picks up cone
+        pickUpCone(3);
+        //sleep(500);
+        //moves arm up
+        goToPose(conePickupX,(conePickupY-8),90,speed);//backs up a bit to clear stack
+        goToPose(45,-29,-135,speed);//goes to place
+        //places cone
+        deployCone(CuriosityPayload.Pole.HIGH);
+        //sleep(300);
+        goToPose(48,10,-180,speed);//back up a bit
+        turnTo(90,speed);
+
+        //go to right
+        goToPoseNoLR(48,24,-180,1);
+    }
 }
+
+//    double[] placeXYA = getCords(CuriosityAutoLeft.Junction.LOWER);
+//    if(coneSide!=3) {
+//        //places preload cone
+//        goToPose(placeXYA[0], placeXYA[1], placeXYA[2], 0.8);
+//        turnTo(-45, speed);
+//        deployCone(CuriosityPayload.Pole.LOW);
+//        //sleep(300);
+//        turnTo(0, speed);
+//        goToPose(55, 0, 0, 1);
+//        goToPose(48, 0, 0, 1);
+//        turnTo(90, speed);
+//    }
+//    double conePickupX = 47;
+//    double conePickupY = 25;
+//    CuriosityAutoLeft.Junction[] order = getOrder(coneSide);
+//    for(int i=0;i<order.length;i++) {
+//        placeXYA = getCords(order[i]);
+//        if (!(i==0&&coneSide==3)){
+//            goToPose(conePickupX, conePickupY, 90, speed);//goes to the stack
+//            //picks up cone
+//            pickUpCone(5 - i);
+//            //sleep(500);
+//            //moves arm up
+//            goToPose(conePickupX, (conePickupY - 10), 90, speed);//backs up a bit to clear stack
+//        }
+//        goToPose(placeXYA[0], placeXYA[1],placeXYA[2],speed);//goes to place
+//        //places cone
+//        deployCone(getHeight(order[i]));
+//        //sleep(300);
+//    }
