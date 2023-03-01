d6d709467c8f1ac5df204c8f900605ed4b4d5d1e
Owen <owencanderson23@gmail.com>
2023-03-01 17:17:08 -0500

    simple autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Curiosity/CuriosityAutoLeft_SIMPLE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_RobotCode/Curiosity/CuriosityAutoLeft_SIMPLE.java
@@ -14,14 +14,13 @@ import org.firstinspires.ftc.teamcode.Core.MechanicalControlToolkit.Basic.BaseRo
 import org.firstinspires.ftc.teamcode.Navigation.Camera;
 import org.firstinspires.ftc.teamcode.Navigation.Odometry.geometry.Pose2d;
 import org.firstinspires.ftc.teamcode.Navigation.OpenCV.OpenCVColors;
-import org.firstinspires.ftc.teamcode.Navigation.UniversalThreeWheelNavigator;
+import org.firstinspires.ftc.teamcode.Navigation.UniversalThreeWheelNavigator.Nav_Axis;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
-import org.firstinspires.ftc.teamcode.Navigation.UniversalThreeWheelNavigator.Nav_Axis;
 
-@Autonomous(name = "*CURIOSITY AUTO LEFT*", group="Curiosity")
+@Autonomous(name = "*SIMPLE CURIOSITY AUTO LEFT*", group="Curiosity")
 @Config
-public class CuriosityAutoLeft extends LinearOpMode {
+public class CuriosityAutoLeft_SIMPLE extends LinearOpMode {
 
     private CuriosityBot robot;
     private boolean isRed;
@@ -37,7 +36,7 @@ public class CuriosityAutoLeft extends LinearOpMode {
     public static double coneSide = 1;
 
     double conePickupX = 45;
-    double conePickupY = 24;
+    double conePickupY = 29;
 
     int conesInStack = 5;
 
@@ -75,139 +74,37 @@ public class CuriosityAutoLeft extends LinearOpMode {
         telemetry.addData("Position", coneSide);
         telemetry.update();
 
-        //places preload cone
-        goToPose(5, -2.3, 0, 0.8);
-        turnTo(-45, speed);
-        deployCone(CuriosityPayload.Pole.LOW);
-        nextLights();
-        //sleep(300);
-        goToPoseOvershoot(5, 0, 0, 0.8, Nav_Axis.ANGLE);
-        moveArmToPickup(conesInStack);
+        //places preload cone on high junction
+        moveConeToPlace(CuriosityPayload.Pole.HIGH);
         goToPoseOvershoot(55, 0, 0, 1,Nav_Axis.X);
         goToPose(48, 0, 0, 1);
+        goToPose(47.5, -3.5, -45, speed);//goes to place
+        turnTo(-45,0.6);
+        //places cone
+        deployCone(CuriosityPayload.Pole.HIGH);
         nextLights();
 
         //1=stack for left, 1=far for right
         //spot 1(green)
         if ((coneSide == 1 && isLeft) || (coneSide == 3 && !isLeft)) {
-            TwoHigh();
-            //TwoMid();
-            OneLow();
-            //go to left
             moveArmToPickup(2);
-            goToPoseNoLR(43, -20, 0, 1);
+            goToPoseNoLR(40, -20, 0, 1);
         }
         //spot 2(purple)
         else if (coneSide == 2) {
-            //OneLow();
-            TwoHigh();
-            TwoMid();
-            //go to center
             moveArmToPickup(2);
-            goToPoseNoLR(43, 0, 0, 1);
+            goToPoseNoLR(40, 0, 0, 1);
         }
         //spot 3(orange)
         else {
-            TwoHigh();
-            //TwoMid();
-            ThreeHigh();
-            //go to right
             moveArmToPickup(2);
-            goToPoseNoLR(43, 24, 0, 1);
+            goToPoseNoLR(40, 24, 0, 1);
         }
         telemetry.addLine("DONE");
         telemetry.update();
         robot.stop();
         robot.getChassis().stop();
         stop();
-    }
-
-    void OneLow() throws InterruptedException {
-        moveArmToPickup(conesInStack);
-        turnTo(90, speed);
-        //raises arm to pick up cone
-        goToStack();
-        //picks up cone
-        pickUpCone(conesInStack);
-        conesInStack --;
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX, (conePickupY - 12), 90, speed);//backs up a bit to clear stack
-        moveConeToPlace(CuriosityPayload.Pole.LOW);
-        goToPose(48, 10, -180, speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.LOW);
-        //sleep(300);
-        goToPose(48, 10, -180, speed);//back up a bit
-    }
-
-    void TwoMid() throws InterruptedException {
-        moveArmToPickup(conesInStack);
-        turnTo(90, speed);
-        //raises arm to pick up cone
-        goToStack();
-        //picks up cone
-        pickUpCone(conesInStack);
-        conesInStack --;
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX, (conePickupY - 12), 90, speed);//backs up a bit to clear stack
-        moveConeToPlace(CuriosityPayload.Pole.MID);
-        goToPose(47, -7, -135, speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.MID);
-        //sleep(300);
-        goToPose(48, -5, -135, speed);//back up a bit
-    }
-
-    void TwoHigh() throws InterruptedException {
-        moveArmToPickup(conesInStack);
-        turnTo(90, speed);
-        //raises arm to pick up cone
-        goToStack();
-        //picks up cone
-        pickUpCone(conesInStack);
-        conesInStack --;
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX, (conePickupY - 12), 90, speed);//backs up a bit to clear stack
-        moveConeToPlace(CuriosityPayload.Pole.HIGH);
-        goToPose(47.5, -3.5, -45, speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.HIGH);
-        //sleep(300);
-        //goToPose(48, 6, 0, speed);//back up a bit
-    }
-
-    void ThreeHigh() throws InterruptedException {
-        moveArmToPickup(conesInStack);
-        turnTo(90, speed);
-        //raises arm to pick up cone
-        goToStack();
-        //picks up cone
-        pickUpCone(conesInStack);
-        conesInStack --;
-        //sleep(500);
-        //moves arm up
-        goToPose(conePickupX, (conePickupY - 12), 90, speed);//backs up a bit to clear stack
-        moveConeToPlace(CuriosityPayload.Pole.HIGH);
-        goToPose(45, -29, -135, speed);//goes to place
-        //places cone
-        deployCone(CuriosityPayload.Pole.LOW);
-        //sleep(300);
-        goToPose(48, -29, -135, speed);//back up a bit
-    }
-
-    public void goToStack() throws InterruptedException {
-        goToPose(conePickupX, conePickupY*sideMultiplier, 90*sideMultiplier, .6);
-//        while (!robot.getPayload().isConeInIntake() && !isStopRequested()) {//&& getRuntime()<28
-//            telemetry.addLine("GOING TO STACK");
-//            robot.navigator.goTowardsPose(conePickupX, conePickupY*sideMultiplier, 90*sideMultiplier, .6);
-//            robot.update();
-//            robot.getPayload().levelGripper();
-//            telemetry.update();
-//        }
-//        robot.getChassis().stop();
     }
 
 
@@ -251,7 +148,7 @@ public class CuriosityAutoLeft extends LinearOpMode {
         robot.getChassis().stop();
     }
 
-    void goToPoseOvershoot(double x, double y, double angle, double speed, UniversalThreeWheelNavigator.Nav_Axis axis) throws InterruptedException {
+    void goToPoseOvershoot(double x, double y, double angle, double speed, Nav_Axis axis) throws InterruptedException {
         Pose2d startPose = robot.navigator.getMeasuredPose();
         double startVal = 0;
         switch (axis){
